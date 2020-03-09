2483cac14ce6aabb44b53dc5df9e85702fcd6575
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 10:45:35 -0700

    Add project files.

---
+++ Assignment 176/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_176
+{
+    public class Employee : Person
+    {
+        public override string SayName()
+        {
+            Console.WriteLine("Employee");
+            return base.SayName();
+        }
+    }
+}
